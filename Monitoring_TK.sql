93cfc0796239ab96b0563ca387dc0881ba06235c
Alvintris <100253781+Alvintris@users.noreply.github.com>
2023-01-06 13:24:46 +0700

    Perubahan Atribut Tabel
    Buku Penghubung ditambah atribut tanggal
    
    Kelas ditambah atribut sesi
    
    Transaksi ditambah atribut tanggal
    
    Penambahan Tabel User

--- Monitoring_TK.sql
+++ Monitoring_TK.sql
@@ -1,90 +1,84 @@
-CREATE TABLE walikelas (
-    nip_walikelas VARCHAR(100) PRIMARY KEY NOT NULL,
-    nama VARCHAR(100),
-    jenis_kelamin VARCHAR(10),
-    no_telp VARCHAR(100)
-);
-
-CREATE TABLE murid (
-    nis_murid VARCHAR(100) PRIMARY KEY NOT NULL,
-    nama_orangtua VARCHAR(100),
-    nama_murid VARCHAR(100),
-    jenis_kelamin VARCHAR(10),
-    umur INT
-);
-
-CREATE TABLE pengajar (
-    nip_pengajar VARCHAR(100) PRIMARY KEY NOT NULL,
-    nama VARCHAR(100),
-    jenis_kelamin VARCHAR(10),
-    no_telp VARCHAR(100)
-);
-
-CREATE TABLE pelajaran (
-    id_pelajaran VARCHAR(100) PRIMARY KEY NOT NULL,
-    nama_pelajaran VARCHAR(100),
-    deskripsi VARCHAR(100)
-);
-
-CREATE TABLE transaksi (
-    id_transaksi VARCHAR(100) PRIMARY KEY NOT NULL,
-    nip_pengajar VARCHAR(100),
-    nis_murid VARCHAR(100),
-    id_pelajaran VARCHAR(100),
-    status_absen BOOLEAN,
-    FOREIGN KEY (nip_pengajar)
-        REFERENCES pengajar (nip_pengajar),
-	FOREIGN KEY (nis_murid)
-        REFERENCES murid (nis_murid),
-	FOREIGN KEY (id_pelajaran)
-        REFERENCES pelajaran (id_pelajaran)
-);
-
-CREATE TABLE detail_transaksi (
-    id_transaksi VARCHAR(100) primary key not null,
-    id_pelajaran VARCHAR(100),
-    FOREIGN KEY (id_transaksi)
-        REFERENCES transaksi (id_transaksi),
-	FOREIGN KEY (id_pelajaran)
-        REFERENCES pelajaran (id_pelajaran)
-);
-
-CREATE TABLE kategori_pelajaran (
-    id_pelajaran VARCHAR(100) PRIMARY KEY NOT NULL,
-    kategori VARCHAR(100),
-    deskripsi VARCHAR(100),
-    FOREIGN KEY (id_pelajaran)
-        REFERENCES pelajaran (id_pelajaran)
-);
-
-
-CREATE TABLE kelas (
-    id_kelas VARCHAR(100) PRIMARY KEY NOT NULL,
-    id_pelajaran VARCHAR(100),
-    nip_walikelas VARCHAR(100),
-    tingkat_kelas INT,
-    nama_kelas VARCHAR(100),
-    jumlah_murid INT,
-    FOREIGN KEY (id_pelajaran)
-        REFERENCES pelajaran (id_pelajaran),
-    FOREIGN KEY (nip_walikelas)
-        REFERENCES walikelas (nip_walikelas)
-);
-
-
-CREATE TABLE buku_penghubung (
-    id_buku VARCHAR(100) PRIMARY KEY NOT NULL,
-    nip_walikelas VARCHAR(100),
-    nis_murid VARCHAR(100),
-    id_pelajaran VARCHAR(100),
-    main_course VARCHAR(100),
-    evaluasi_orangtua VARCHAR(100),
-    catatan_guru VARCHAR(100),
-    snack VARCHAR(100),
-    FOREIGN KEY (nip_walikelas)
-        REFERENCES walikelas (nip_walikelas),
-    FOREIGN KEY (nis_murid)
-        REFERENCES murid (nis_murid),
-    FOREIGN KEY (id_pelajaran)
-        REFERENCES pelajaran (id_pelajaran)
-);
+CREATE TABLE user(
+    ID_User INT NOT NULL PRIMARY KEY,
+    Username VARCHAR(100) NOT NULL,
+    Password VARCHAR(100) NOT NULL,
+    Role VARCHAR(100) NOT NULL
+    );
+    
+CREATE TABLE pelajaran(
+    ID_Pelajaran VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nama_Pelajaran VARCHAR(100),
+    Deskripsi TEXT
+    );
+    
+CREATE TABLE murid(
+    NIS_Murid VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nama_Murid VARCHAR(100) NOT NULL,
+    Jenis_Kelamin VARCHAR(100) NOT NULL,
+    Umur INT NOT NULL,
+    Nama_OrangTua VARCHAR(100)
+    );
+    
+CREATE TABLE pengajar(
+    NIP_Pengajar VARCHAR(100) NOT NULL PRIMARY KEY,
+    Nama_Pengajar VARCHAR(100) NOT NULL,
+    Jenis_Kelamin VARCHAR(20) NOT NULL,
+    NO_Telepon VARCHAR(20)
+    );
+    
+CREATE TABLE kategori_pelajaran(
+    ID_Pelajaran VARCHAR(100) NOT NULL PRIMARY KEY,
+    Kategori VARCHAR(100) NOT NULL,
+    Deskripsi TEXT,
+    FOREIGN KEY(ID_Pelajaran) REFERENCES pelajaran(ID_Pelajaran)
+    );
+    
+CREATE TABLE walikelas(
+    NIP_Walikelas VARCHAR(100) NOT NULL PRIMARY KEY,
+    NIP_Pengajar VARCHAR(100) NOT NULL,
+    FOREIGN KEY(NIP_Pengajar) REFERENCES pengajar(NIP_Pengajar)
+    );
+    
+CREATE TABLE kelas(
+    ID_Kelas VARCHAR(100) NOT NULL PRIMARY KEY,
+    NAMA_Kelas VARCHAR(100) NOT NULL,
+    ID_Pelajaran VARCHAR(100) NOT NULL,
+    NIP_Walikelas VARCHAR(100) NOT NULL,
+    Tingkat_Kelas INT,
+    Jumlah_Murid INT,
+    Sesi INT,
+    FOREIGN KEY(ID_Pelajaran) REFERENCES pelajaran(ID_Pelajaran),
+    FOREIGN KEY(NIP_Walikelas) REFERENCES walikelas(NIP_Walikelas)
+    );
+    
+CREATE TABLE transaksi(
+    ID_Transaksi VARCHAR(100) NOT NULL PRIMARY KEY,
+    NIP_Pengajar VARCHAR(100) NOT NULL,
+    NIS_Murid VARCHAR(100) NOT NULL,
+    ID_Pelajaran VARCHAR(100) NOT NULL,
+    Status_Absen VARCHAR(100) DEFAULT '-',
+    Tanggal DATE,
+    FOREIGN KEY(NIP_Pengajar) REFERENCES pengajar(NIP_Pengajar),
+    FOREIGN KEY(NIS_Murid) REFERENCES murid(NIS_Murid),
+    FOREIGN KEY(ID_Pelajaran) REFERENCES pelajaran(ID_Pelajaran)
+    );
+    
+CREATE TABLE buku_penghubung(
+    ID_Buku VARCHAR(100) NOT NULL PRIMARY KEY,
+    NIS_Murid VARCHAR(100) NOT NULL,
+    ID_Pelajaran VARCHAR(100) NOT NULL,
+    NIP_Walikelas VARCHAR(100) NOT NULL,
+    Main_Course VARCHAR(100),
+    Catatan_Guru TEXT,
+    Tanggal DATE,
+    FOREIGN KEY(NIS_Murid) REFERENCES murid(NIS_Murid),
+    FOREIGN KEY(ID_Pelajaran) REFERENCES pelajaran(ID_Pelajaran),
+    FOREIGN KEY(NIP_Walikelas) REFERENCES walikelas(NIP_Walikelas)
+    );
+    
+CREATE TABLE detail_transaksi(
+    ID_Transaksi VARCHAR(100) NOT NULL PRIMARY KEY,
+    ID_Pelajaran VARCHAR(100) NOT NULL PRIMARY KEY,
+    FOREIGN KEY(ID_Transaksi) REFERENCES transaksi(ID_Transaksi),
+    FOREIGN KEY(ID_Pelajaran) REFERENCES pelajaran(ID_Pelajaran)
+    );
